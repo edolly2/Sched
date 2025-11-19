d3ab9321f56dafeee4ec4a52cb68cb381466faf3
edolly2 <edolly2@protonmail.com>
2025-11-18 19:57:02 -0600

    ""

--- src/Scheduler.jsx
+++ src/Scheduler.jsx
@@ -136,7 +136,7 @@ const initialPeople = [
 
 // RULES
 const rules = {
-  p1: ["mon-2"], // example, adjust if you want
+  p1: ["ank-sun-1, ank-mon-1, ank-tue-1, ank-thur-1, ank-fri-1, ank-sat-1,"], // example, adjust if you want
   p2: ["mon-1", "tue-1"],
 };
 
